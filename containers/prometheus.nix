666aab8b9192c5f73a33ef1872d0c710e13f4f31
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-20 14:56:23 -0500

    Update prometheus.nix

--- containers/prometheus.nix
+++ containers/prometheus.nix
@@ -15,7 +15,7 @@
       "traefik.http.routers.prometheus-secure.entrypoints" = "https";
       "traefik.http.routers.prometheus-secure.middlewares" = "traefik-https-redirect";
       "traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme" = "https";
-      "traefik.http.routers.prometheus-secure.rule" = "Host(`subdomain.domain.com.com`)";
+      "traefik.http.routers.prometheus-secure.rule" = "Host(`subdomain.domain.com`)";
       "traefik.http.routers.prometheus-secure.service" = "prometheus";
       "traefik.http.routers.prometheus-secure.tls" = "true";
       "traefik.http.routers.prometheus-secure.tls.certresolver" = "myresolver"

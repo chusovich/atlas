e5b0d16232d863e8746394224a51a2f2d6aa8384
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-20 14:51:41 -0500

    Update beszel-hub.nix

--- containers/beszel-hub.nix
+++ containers/beszel-hub.nix
@@ -1,27 +1,28 @@
-services:
-  beszel-hub:
-    image: henrygd/beszel:0.18.2
-    container_name: beszel-hub
-    restart: unless-stopped
-    # ports:
-    #  - 8090:8090
-    volumes:
-      - /home/atlas/app-data/beszel-hubdata:/beszel_data
-      - /home/atlas/app-data/beszel-hub/socket:/beszel_socket
-    networks:
-      traefik: 
-    labels:
-      traefik.enable: true
-      # HTTP
-      traefik.http.routers.beszel-hub.entrypoints: http
-      traefik.http.routers.beszel-hub.rule: Host(`beszel.services.husovich.com`)
-      traefik.http.routers.beszel-hub.middlewares: traefik-https-redirect
-      traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme: https
-      # HTTPS
-      traefik.http.routers.beszel-hub-secure.entrypoints: https
-      traefik.http.routers.beszel-hub-secure.rule: Host(`beszel.services.husovich.com`)
-      traefik.http.routers.beszel-hub-secure.tls.certresolver: myresolver
+{ config, pkgs, ... }:
 
-networks:
-  traefik:
-    external: true
+{
+  virtualisation.oci-containers.containers.beszel-hub = {
+    image = "henrygd/beszel:0.18.2";
+    hostname = "beszel-hub";
+    autoStart = true;
+    volumes = [
+      "/home/atlas/app-data/beszel-hubdata:/beszel_data"
+      "/home/atlas/app-data/beszel-hub/socket:/beszel_socket"
+    ];
+    networks = [
+      "traefik"
+    ];
+    labels = {
+      label=traefik.enable=true"
+      # HTTP
+      "--label=traefik.http.routers.beszel-hub.entrypoints=http"
+      "--label=traefik.http.routers.beszel-hub.rule=Host(`beszel.services.husovich.com`)"
+      "--label=traefik.http.routers.beszel-hub.middlewares=traefik-https-redirect"
+      "--label=traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme=https"
+      # HTTPS
+      "--label=traefik.http.routers.beszel-hub-secure.entrypoints=https"
+      "--label=traefik.http.routers.beszel-hub-secure.rule=Host(`beszel.services.husovich.com`)"
+      "--label=traefik.http.routers.beszel-hub-secure.tls.certresolver=myresolver"
+    ];
+  };
+}

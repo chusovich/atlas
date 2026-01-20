b96503c41dec1da5e545f671b1cc2aaf56b71dd3
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-20 14:42:25 -0500

    Update and rename containers/prometheus/compose.yml to containers/prometheus.nix

---
+++ containers/prometheus.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ...}:
+
+{
+  virtualisation.oci-containers.backend = "docker";
+  virtualization.oci-containers.containers.prometheus = { 
+    image = "prom/prometheus:v3.9.1";
+    hostname = "prometheus";
+    autoStart = true;
+    volumes = [
+      "/hone/atlas/prometheus.yml:/etc/prometheus/prometheus.yml"
+    ];
+    labels = {
+      "traefik.enable" = "true;"
+      "traefik.docker.network" = "traefik";
+      "traefik.http.routers.prometheus-secure.entrypoints" = "https";
+      "traefik.http.routers.prometheus-secure.middlewares" = "traefik-https-redirect";
+      "traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme" = "https";
+      "traefik.http.routers.prometheus-secure.rule" = "Host(`prometheus.services.husovich.com`)";
+      "traefik.http.routers.prometheus-secure.service" = "prometheus";
+      "traefik.http.routers.prometheus-secure.tls" = "true";
+      "traefik.http.routers.prometheus-secure.tls.certresolver" = "myresolver"
+      "traefik.http.routers.prometheus.entrypoints" = "http";
+      "traefik.http.routers.prometheus.rule" = "Host(`prometheus.services.husovich.com`)
+      "traefik.http.services.prometheus.loadbalancer.server.port" = "9090";
+    };
+  };
+}

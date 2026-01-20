f41d383e2a4c0cbba59b62ee4509fbbdbfb1c6e1
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-20 14:54:26 -0500

    Update beszel-hub.nix

--- containers/beszel-hub.nix
+++ containers/beszel-hub.nix
@@ -13,16 +13,18 @@
       "traefik"
     ];
     labels = {
-      label=traefik.enable=true"
-      # HTTP
-      "--label=traefik.http.routers.beszel-hub.entrypoints=http"
-      "--label=traefik.http.routers.beszel-hub.rule=Host(`beszel.services.husovich.com`)"
-      "--label=traefik.http.routers.beszel-hub.middlewares=traefik-https-redirect"
-      "--label=traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme=https"
-      # HTTPS
-      "--label=traefik.http.routers.beszel-hub-secure.entrypoints=https"
-      "--label=traefik.http.routers.beszel-hub-secure.rule=Host(`beszel.services.husovich.com`)"
-      "--label=traefik.http.routers.beszel-hub-secure.tls.certresolver=myresolver"
-    ];
+      "traefik.enable" = "true;"
+      "traefik.docker.network" = "traefik";
+      "traefik.http.routers.beszel-secure.entrypoints" = "https";
+      "traefik.http.routers.beszel-secure.middlewares" = "traefik-https-redirect";
+      "traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme" = "https";
+      "traefik.http.routers.beszel-secure.rule" = "Host(`subdomain.domain.com`)";
+      "traefik.http.routers.beszel-secure.service" = "beszel";
+      "traefik.http.routers.beszel-secure.tls" = "true";
+      "traefik.http.routers.beszel-secure.tls.certresolver" = "myresolver"
+      "traefik.http.routers.beszel.entrypoints" = "http";
+      "traefik.http.routers.beszel.rule" = "Host(`subdomain.domain.com`)";
+      "traefik.http.services.beszel.loadbalancer.server.port" = "8090";
+    };
   };
 }

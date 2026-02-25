54a44ce65b7095a330289904ace7f1128a5166aa
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-02-25 13:43:25 -0500

    Add Grafana service configuration

---
+++ services/grafana.nix
@@ -0,0 +1,16 @@
+{ config, lib, ... }:
+
+{
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = 3000;
+        enforce_domain = false;
+        enable_gzip = true;
+        domain = "grafana.services.husovich.com";
+      };
+    };
+  };
+}

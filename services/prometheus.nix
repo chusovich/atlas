3119e5f4145ab0cf0daba6df11f25188e5ed46f1
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-02-25 13:11:38 -0500

    Update Prometheus scrape interval and job name

--- services/prometheus.nix
+++ services/prometheus.nix
@@ -1,14 +1,19 @@
-{ config, lib, pgks, ...}:
+{ config, pkgs, ... }:
 
 {
+  # https://wiki.nixos.org/wiki/Prometheus
+  # https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters-configuration
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/default.nix
   services.prometheus = {
     enable = true;
-    globalConfig.scrape_interval = "30s";
+    globalConfig.scrape_interval = "10s"; # "1m"
     scrapeConfigs = [
       {
-        job_name = "localhost";
+        job_name = "node";
         static_configs = [{
           targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
       }
     ];
+  };
 }
